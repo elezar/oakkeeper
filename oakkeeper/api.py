7c164df600dc4aa9b27152c29a9fcedb17f0fb60
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-09-26 11:52:19 +0200

    #19 leave api as-is to avoid breaking change

--- oakkeeper/api.py
+++ oakkeeper/api.py
@@ -51,6 +51,44 @@ def get_repos(base_url, token, page=0):
     return r.json()
 
 
+def get_branch_data(base_url, token, repo, branch):
+    url = base_url + '/repos/{repo}/branches/{branch}'.format(repo=repo, branch=branch)
+    headers = {'Accept': 'application/vnd.github.loki-preview+json'}
+    auth = HTTPBasicAuth('token', token)
+    r = requests.get(url, headers=headers, auth=auth)
+    r.raise_for_status()
+    return r.json()
+
+
+def protect_branch(base_url, token, repo, branch, required_contexts):
+    protection_payload = {
+        'protection': {
+            'enabled': True,
+            'required_status_checks': {
+                'enforcement_level': 'everyone',
+                'contexts': required_contexts
+            }
+        }
+    }
+    url = base_url + '/repos/{repo}/branches/{branch}'.format(repo=repo, branch=branch)
+    headers = {'Accept': 'application/vnd.github.loki-preview+json'}
+    auth = HTTPBasicAuth('token', token)
+    r = requests.patch(
+        url,
+        headers=headers,
+        auth=auth,
+        data=json.dumps(protection_payload))
+    r.raise_for_status()
+
+
+def ensure_branch_protection(base_url, token, repo, branch='master'):
+    branch_data = get_branch_data(base_url, token, repo, branch)
+    contexts = branch_data['protection']['required_status_checks']['contexts']
+    if 'zappr' not in contexts:
+        contexts.append('zappr')
+        protect_branch(base_url, token, repo, branch, contexts)
+
+
 def get_commits(base_url, token, repo):
     url = base_url + '/repos/{repo}/commits'.format(repo=repo)
     auth = HTTPBasicAuth('token', token)
